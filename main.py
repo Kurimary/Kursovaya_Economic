5e51bca939cd7708eb029a3b0b795e454b887f0e
Thevdoo-PC\Thevdoo <suwigor@yandex.ru>
2019-12-11 03:35:54 +0700

    Finish with Credit_func and all type of revenue

--- main.py
+++ main.py
@@ -2,17 +2,23 @@ from openpyxl import load_workbook
 wb_val = load_workbook(filename='ExcelDocument.xlsx', data_only=True)
 sheet_val = wb_val['Общая информация']
 def Revenue_all_type():
+    Credit_func = Credit()
+    Credit_func.append(0)
+    Credit_func.append(0)
     Revenue_wo = []  # Выручка
     Revenue_wt = []  # Выручка минус налог
     Gross_profit = []  # Валовая прибыль
     Revenue_for_sales = []  # Прибыль от продаж
+    Profit_before_tax = []
     Pure_profit =[]
     for i in range(5):
         Revenue_wo.append(Max_production * Obyom_proizv[i] * Cost_per_unit)
         Revenue_wt.append(Revenue_wo[i] * (1 - Nalogovaya_nagruzka))
-        Gross_profit.append(Revenue_wt[i] - (Max_production * Obyom_proizv[i] * (Cost_per_unit - Sebe_stoim)))
+        Gross_profit.append(Revenue_wt[i] - (Max_production * Obyom_proizv[i] * (Cost_per_unit - Sebe_stoim))-Amortization)
         Revenue_for_sales.append(Gross_profit[i] - (Komm_rasx + Uprav_rasx))
-        #Pure_profit.append()
+        Profit_before_tax.append(Revenue_for_sales[i]-Credit_func[i])
+        Pure_profit.append(Profit_before_tax[i]*(1-Stavka_po_nalogu))
+    print(Pure_profit)
 
 def Credit():
     Dolg = []
@@ -20,17 +26,20 @@ def Credit():
     Balance = []
     Percent = []
     Ostatok = []
-    Op_def = (Operation_capital()[5])
+    Op_def = (Operation_capital()[5]/1000)
     Credit_first_point = Stoimost_oborud - Akzion_finan_of_project
-    Dolg.append(Credit_first_point+Op_def*(1+Stavka_po_kreditu))
+    Dolg.append((Credit_first_point+Op_def)*(1+Stavka_po_kreditu))
     Percent.append((Credit_first_point+Op_def)*Stavka_po_kreditu)
     Payment.append((Credit_first_point+Op_def)*Pogashenie_dolg_kred[0]+Percent[0])
     Ostatok.append(Dolg[0]-Payment[0])
-    Ostatok.append((Credit_first_point*Op_def)*Pogashenie_dolg_kred[0])
+    Ostatok.append((Credit_first_point+Op_def)*Pogashenie_dolg_kred[1])
     Percent.append(Ostatok[0]*Stavka_po_kreditu)
     Payment.append((Credit_first_point+Op_def)*Pogashenie_dolg_kred[1]+Percent[1])
-    print(Payment[1],Credit_first_point,Op_def,Pogashenie_dolg_kred[0],Percent[0])
-
+    Dolg.append(Ostatok[0])
+    Dolg.append(Ostatok[1])
+    Percent.append(Ostatok[1]*Stavka_po_kreditu)
+    Payment.append(Dolg[2]+Percent[2])
+    return (Payment)
 
 def Operation_capital():
     S_and_M_Per_Year = []
@@ -67,19 +76,18 @@ def Operation_capital():
     for k in range(5):
         Summ.append(Diffe_value[k][0]+Diffe_value[k][1]+Diffe_value[k][2]+Diffe_value[k][3]-Diffe_value[k][4])
     Summ.append(S_and_M_Per_Year[0]+NZP[0]+ZGP[0]+DZ[0]+KZ[0])
-    print(Summ[5])
     return(Summ)
 
         # Общая информация
-Stoimost_oborud = (sheet_val['B2'].value)*1000000
+Stoimost_oborud = (sheet_val['B2'].value)*(10**6)
 Srok_pol_ispolz = sheet_val['B3'].value
-Max_production = sheet_val['B4'].value
-Akzion_finan_of_project = (sheet_val['B5'].value)*1000000
+Max_production = sheet_val['B4'].value*1000
+Akzion_finan_of_project = (sheet_val['B5'].value)*(10**6)
 Trebue_doxodn = sheet_val['B6'].value
 Stavka_po_kreditu = sheet_val['B7'].value
 Stavka_po_nalogu = sheet_val['B8'].value
 Nalogovaya_nagruzka = sheet_val['B9'].value
-Amortization = 4000
+Amortization = 4*(10**6)
 index = ['B', 'C', 'D', 'E', 'F']
 Obyom_proizv = []
 Ostatochn_st_oborud = []
@@ -94,8 +102,8 @@ Sebe_stoim = sheet_val['B19'].value
 S_and_M  = sheet_val['B21'].value
 ZP = sheet_val['B22'].value
 Others = sheet_val['B23'].value
-Komm_rasx = sheet_val['B24'].value
-Uprav_rasx = sheet_val['B25'].value
+Komm_rasx = sheet_val['B24'].value*(10**6)
+Uprav_rasx = sheet_val['B25'].value*(10**6)
 Summa_rasxodov = (Komm_rasx+Uprav_rasx)*1000
 
 #Период оборота (в днях)
